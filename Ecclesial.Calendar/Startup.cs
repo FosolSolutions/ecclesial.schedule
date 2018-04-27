7f0c0b24ac681157cefabbf25a72daab87d52e47
fosol <jeremymfoster@hotmail.com>
2018-04-27 16:43:43 -0700

    Cleaning up code

--- Ecclesial.Calendar/Startup.cs
+++ Ecclesial.Calendar/Startup.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Ecclesial.Calendar.DAL;
 using Ecclesial.Calendar.DAL.Extensions;
 using Ecclesial.Calendar.Filters;
 using Fosol.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
